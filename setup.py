c8233d43d3678afbd2944b9f856427031200d8a9
Edoardo Cignoni <edoardo.cignoni96@gmail.com>
2022-11-05 18:57:11 +0100

    Downloaded chemical hardnesses, stored as a dictionary.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='stda_pyscf',
+    version='0.0.1',
+    description='Tiny implementation of sTDA with PySCF',
+    #long_description=open('README.md').read()
+    author='Edoardo Cignoni',
+    author_email='edoardo.cignoni96@gmail.com',
+)
