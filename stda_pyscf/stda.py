666935af899f40a86c5c7f5c5410e027a1cf8941
Edoardo Cignoni <edoardo.cignoni96@gmail.com>
2022-11-08 15:51:50 +0100

    Added function to evaluate the active space

--- stda_pyscf/stda.py
+++ stda_pyscf/stda.py
@@ -64,7 +64,7 @@ def gamma_J(mol, ax, beta=None):
     eta = hardness_matrix(mol)
     if beta is None:
         _, beta = get_alpha_beta(ax)
-    denom = ((R * ax * eta)**beta + 1) ** (1./beta)
+    denom = ((R * ax * eta) ** beta + 1) ** (1.0 / beta)
     gamma = ax * eta / denom
     # gamma = (1.0 / (R**beta + (ax * eta) ** (-beta))) ** (1.0 / beta)
     return gamma
@@ -75,7 +75,7 @@ def gamma_K(mol, ax, alpha=None):
     eta = hardness_matrix(mol)
     if alpha is None:
         alpha, _ = get_alpha_beta(ax)
-    denom = ((R * eta)**alpha + 1) ** (1./alpha)
+    denom = ((R * eta) ** alpha + 1) ** (1.0 / alpha)
     gamma = eta / denom
     # gamma = (1.0 / (R**alpha + eta ** (-alpha))) ** (1.0 / alpha)
     return gamma
@@ -98,7 +98,6 @@ def eri_mo_monopole(mf, alpha=None, beta=None, ax=None):
     mo_coeff = mf.mo_coeff
     if ax is None:
         ax = get_hybrid_coeff(mf)
-    print(ax)
     gam_J = gamma_J(mol, ax, beta)
     gam_K = gamma_K(mol, ax, alpha)
     q = charge_density_monopole(mol, mo_coeff)
@@ -107,12 +106,85 @@ def eri_mo_monopole(mf, alpha=None, beta=None, ax=None):
     return eri_J, eri_K
 
 
+def _select_active_space(a, eri_J, eri_K, e_max, tp):
+    nocc, nvir, _, _ = a.shape
+    # (1) select the CSFs by energy
+    # E_u = A_ia,ia = (e_a - e_i) + 2*(ia|ia) - (ii|aa) ; u = ia
+    diag_a = np.diag(a.reshape(nocc * nvir, nocc * nvir)).copy()
+    diag_a += np.einsum("iaia->ia", eri_K).reshape(-1) * 2
+    diag_a -= np.einsum("iaia->ia", eri_J).reshape(-1)
+    # Select P-CSF and N-CSF
+    idx_pcsf = np.where(diag_a <= e_max)[0]
+    idx_ncsf = np.where(diag_a > e_max)[0]
+    pcsf = np.concatenate([[divmod(i, nvir)] for i in idx_pcsf])
+    ncsf = np.concatenate([[divmod(i, nvir)] for i in idx_ncsf])
+    # (2) select the S-CSFs by perturbation
+    eri_J = eri_J[:, :, pcsf[:, 0], pcsf[:, 1]].reshape(nocc * nvir, pcsf.shape[0])
+    eri_K = eri_K[:, :, pcsf[:, 0], pcsf[:, 1]].reshape(nocc * nvir, pcsf.shape[0])
+    a_uv = 2 * eri_K - eri_J
+    denom = diag_a[:, None] - diag_a[idx_pcsf]
+    # P-CSF have e_perturb = np.infty
+    e_perturb = np.sum(
+        np.divide(
+            np.abs(a_uv) ** 2,
+            denom,
+            out=np.full(a_uv.shape, np.infty),
+            where=denom != 0,
+        ),
+        axis=1,
+    )
+    # P-CSF + S-CSF
+    idx_pcsf = np.where(e_perturb >= tp)[0]
+    idx_ncsf = np.where(e_perturb < tp)[0]
+    pcsf = np.concatenate([[divmod(i, nvir)] for i in idx_pcsf])
+    ncsf = np.concatenate([[divmod(i, nvir)] for i in idx_ncsf])
+    # Get perturbative contribution to sum to P-CSF
+    e_ncsf = e_perturb[idx_ncsf].sum()
+    return pcsf, e_ncsf
+
+
+def select_active_space(
+    mf,
+    a=None,
+    eri_J=None,
+    eri_K=None,
+    e_max=7.0,
+    tp=1e-4,
+    alpha=None,
+    beta=None,
+    ax=None,
+):
+    if a is None:
+        mo_energy = mf.mo_energy
+        mo_occ = mf.mo_occ
+        occidx = np.where(mo_occ == 2)[0]
+        viridx = np.where(mo_occ == 0)[0]
+        nocc = len(occidx)
+        nvir = len(viridx)
+        e_ia = lib.direct_sum("a-i->ia", mo_energy[viridx], mo_energy[occidx])
+        a = np.diag(e_ia.ravel()).reshape(nocc, nvir, nocc, nvir)
+    if eri_J is None or eri_K is None:
+        _eri_J, _eri_K = eri_mo_monopole(mf, alpha=alpha, beta=beta, ax=ax)
+        _eri_J = np.einsum('ijab->iajb', _eri_J[:nocc, :nocc, nocc:, nocc:])
+        _eri_K = np.einsum('iajb->iajb', _eri_K[:nocc, nocc:, :nocc, nocc:])
+        if eri_J is None:
+            eri_J = _eri_J
+        if eri_K is None:
+            eri_K = _eri_K
+    e_max = e_max / AU_TO_EV
+    pcsf, e_ncsf = _select_active_space(
+        a=a, eri_J=eri_J, eri_K=eri_K, e_max=e_max, tp=tp
+    )
+    return pcsf, e_ncsf
+
+
 def get_ab(
     mf, mo_energy=None, mo_coeff=None, mo_occ=None, alpha=None, beta=None, ax=None
 ):
     r"""A and B matrices for sTDA response function.
 
     A[i,a,j,b] = \delta_{ab}\delta_{ij}(E_a - E_i) + 2(ia|jb)' - (ij|ab)'
+    B[i,a,j,b] = 0
     """
     if mo_energy is None:
         mo_energy = mf.mo_energy
@@ -146,9 +218,8 @@ def get_ab(
 
 def direct_diagonalization(a, nstates=3):
     nocc, nvir, _, _ = a.shape
-    a = a.reshape(nocc*nvir, nocc*nvir)
+    a = a.reshape(nocc * nvir, nocc * nvir)
     e, v = np.linalg.eig(a)
-    e = np.sort(e[e>0])[:nstates]
+    e = np.sort(e[e > 0])[:nstates]
     e *= AU_TO_EV
     return e
-
